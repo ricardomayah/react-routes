c2bd0c5f6f8a738a3bd35528b1de49987ca30116
ricardomayah <ricardomayah@gmail.com>
2020-09-28 12:27:10 -0500

    Se crea menu de navegacion

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,43 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  NavLink
 } from 'react-router-dom'
+import { Contacto } from './components/Contacto';
+import { Inicio } from './components/Inicio';
+import { Nosotros } from './components/Nosotros';
 
 function App() {
   return (
     <Router>
-      <div className="container">
-        <h1>Hola</h1>
-        <hr/>
+      <div className="container mt-5">
+        <div className="btn-group">
+          <Link to="/" className="btn btn-dark">
+            Inicio
+          </Link>
+          <Link to="/nosotros" className="btn btn-dark">
+            Nosotros
+          </Link>
+          <NavLink to="/contacto" className="btn btn-dark" activeClassName="active">
+            Contacto
+          </NavLink>
+        </div>
+        <hr />
         <Switch>
-          <Route path="/contacto">
-            Esta es un apagina de contacto
+          <Route path="/" exact>
+            <Inicio />
           </Route>
           <Route path="/contacto">
-            Esta es un apagina de contacto
+            <Contacto />
           </Route>
-          <Route path="/contacto">
-            Esta es un apagina de contacto
+          <Route path="/nosotros">
+            <Nosotros />
           </Route>
+
         </Switch>
       </div>
-    </Router> 
+    </Router>
   );
 }
 
---
+++ src/components/Nosotros.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Nosotros = () => {
+    return (
+        <div>
+            <h1>Nosotros</h1>
+        </div>
+    )
+}
