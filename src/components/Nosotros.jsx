cf26bf044eb63ce9409c5e5485a9534088b01d50
ricardomayah <ricardomayah@gmail.com>
2020-10-02 14:56:52 -0500

    Uso del hook useEffect

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React , {useEffect} from 'react'
 
 export const Nosotros = () => {
+
+    const [equipo,setEquipo] = React.useState([])
+
+    useEffect(() => {
+        console.log("desde useeffect")
+        obtenerDatos()
+    },[])
+
+    const obtenerDatos = async () => {
+        const data  = await fetch("https://jsonplaceholder.typicode.com/users")
+        const users = await data.json()
+        setEquipo(users)
+    }
+
     return (
         <div>
             <h1>Nosotros</h1>
+            <ul>
+                {
+                    equipo.map(item => {
+                        return <li key={item.id}>{item.name}</li>   
+                    })
+                }
+            </ul>
         </div>
     )
 }
